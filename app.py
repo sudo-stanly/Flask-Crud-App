6456b5507c76129ef49eafde39e0eb452a4bb846
sudo-stanly <alys.git24@gmail.com>
2026-01-18 11:44:39 +0800

    setup app assign flask to app, add method and decorator and create route with directory, and assign name is in main file, run app and add debugger to true for flask to update itself

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from flask import Flask
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
+# My App
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Testing 123"
+
+if __name__ in "__main__":
+    app.runb(debugger=True)
