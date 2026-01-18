1761bcc301dd71054b8e4721db1898b23e9e9116
sudo-stanly <alys.git24@gmail.com>
2026-01-18 12:51:30 +0800

    removed space from html, configured sqlalchemy model

--- app.py
+++ app.py
@@ -2,9 +2,23 @@
 from flask import (Flask, render_template)
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # My App
 app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database.db"
+db = SQLAlchemy(app)
+
+# Data Class ~ row of data
+class myTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer,default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 
 @app.route("/")
 def index():
